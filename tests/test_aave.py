db491c7d2c00ab26c396e8c753df6162162a025f
spaceh3ad <jan.kwiatkowski@studenci.wsksim.edu.pl>
2021-12-13 18:33:57 +0100

    added pytest fixtures

--- tests/test_aave.py
+++ tests/test_aave.py
@@ -1,17 +1,32 @@
 from great_ape_safe import GreatApeSafe
+import pytest
 
 
-def test_deposit():
-    safe = GreatApeSafe('dev.badgerdao.eth')
-    usdc = safe.contract('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48')
-    ausdc = safe.contract('0xBcca60bB61934080951369a648Fb03DF4F96263C')
+@pytest.fixture
+def safe():
+    return GreatApeSafe("dev.badgerdao.eth")
 
+
+@pytest.fixture
+def usdc(safe):
+    return safe.contract("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
+
+
+@pytest.fixture
+def ausdc(safe):
+    return safe.contract("0xBcca60bB61934080951369a648Fb03DF4F96263C")
+
+
+def test_deposit(safe, usdc, ausdc):
     bal_before_usdc = usdc.balanceOf(safe)
     bal_before_ausdc = ausdc.balanceOf(safe)
-    to_deposit = 100_000 * 10**usdc.decimals()
+    to_deposit = 100_000 * 10 ** usdc.decimals()
 
     safe.init_aave()
     safe.aave.deposit(usdc, to_deposit)
 
     assert usdc.balanceOf(safe) == bal_before_usdc - to_deposit
     assert ausdc.balanceOf(safe) == bal_before_ausdc + to_deposit
+
+
+# def test_withdraw(safe):
