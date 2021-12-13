f0983a5180f9b3a66307eff81d094bc086714487
spaceh3ad <jan.kwiatkowski@studenci.wsksim.edu.pl>
2021-12-14 00:02:53 +0100

    added support for testing multiple assets

--- tests/test_aave.py
+++ tests/test_aave.py
@@ -1,32 +1,26 @@
-from great_ape_safe import GreatApeSafe
 import pytest
+from conftest import native, aave, safe
 
 
-@pytest.fixture
-def safe():
-    return GreatApeSafe("dev.badgerdao.eth")
+@pytest.mark.parametrize(
+    "native_asset, aave_asset", zip(native.items(), aave.items()), indirect=True
+)
+def test_deposits(safe, native_asset, aave_asset):
+    _native_asset = safe.contract(native_asset[1])
+    _aave_asset = safe.contract(aave_asset[1])
 
-
-@pytest.fixture
-def usdc(safe):
-    return safe.contract("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
-
-
-@pytest.fixture
-def ausdc(safe):
-    return safe.contract("0xBcca60bB61934080951369a648Fb03DF4F96263C")
-
-
-def test_deposit(safe, usdc, ausdc):
-    bal_before_usdc = usdc.balanceOf(safe)
-    bal_before_ausdc = ausdc.balanceOf(safe)
-    to_deposit = 100_000 * 10 ** usdc.decimals()
+    print(f"Testing for {native_asset[0]}/{aave_asset[0]}")
+    bal_before_native_asset_deposit = _native_asset.balanceOf(safe)
+    bal_before_aave_asset_deposit = _aave_asset.balanceOf(safe)
+    print(f"{native_asset[0]}:{_native_asset.decimals()}")
+    to_deposit = 100_000 * 10 ** _native_asset.decimals()
 
     safe.init_aave()
-    safe.aave.deposit(usdc, to_deposit)
+    safe.aave.deposit(_native_asset, to_deposit)
 
-    assert usdc.balanceOf(safe) == bal_before_usdc - to_deposit
-    assert ausdc.balanceOf(safe) == bal_before_ausdc + to_deposit
+    assert _native_asset.balanceOf(safe) == bal_before_native_asset_deposit - to_deposit
+    assert _aave_asset.balanceOf(safe) == bal_before_aave_asset_deposit + to_deposit
 
 
-# def test_withdraw(safe):
+# def test_withdraw(safe, usdc, ausdc):
+#     safe.aave.withdraw(usdc, 100_000 * 10 ** usdc.decimals())
